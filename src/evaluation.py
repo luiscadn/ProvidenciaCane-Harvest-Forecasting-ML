79129a99bff0bdaf82d122de7c61f5288c27a15d
Melissa-hb <meli04230@gmail.com>
2026-04-25 14:54:35 -0500

    Implementation of changes to the classification models based on the results obtained from the EDA.

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,4 +1,3 @@
-from matplotlib import cm
 from sklearn.metrics import confusion_matrix, f1_score, cohen_kappa_score
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -7,13 +6,17 @@ import seaborn as sns
 def calcular_metricas(y_true, y_pred):
     macro_f1 = f1_score(y_true, y_pred, average='macro')
     kappa = cohen_kappa_score(y_true, y_pred)
+
     print("--- REPORTE DE RESULTADOS ---")
     print(f"F1-Score (Macro) : {macro_f1:.4f}")
     print(f"Cohen's Kappa    : {kappa:.4f}")
 
+    return macro_f1, kappa
+
 
 def graficar_confusion(y_true, y_pred, labels_target):
     cm = confusion_matrix(y_true, y_pred, labels=labels_target)
+
     plt.figure(figsize=(6, 4))
     sns.heatmap(
         cm,
@@ -23,14 +26,17 @@ def graficar_confusion(y_true, y_pred, labels_target):
         xticklabels=labels_target,
         yticklabels=labels_target
     )
-    plt.title("Matriz de Confusión de la Cosecha")
+
+    plt.title("Matriz de Confusión de la Predicción")
     plt.ylabel("Valor Real (Campo)")
     plt.xlabel("Predicción del Modelo")
     plt.show()
 
+    return cm
+
 
 def evaluar_modelo(y_true, y_pred, labels_target=['Bajo', 'Medio', 'Alto']):
     macro_f1, kappa = calcular_metricas(y_true, y_pred)
-    graficar_matriz_confusion(y_true, y_pred, labels_target)
+    cm = graficar_confusion(y_true, y_pred, labels_target)
 
     return macro_f1, kappa, cm
